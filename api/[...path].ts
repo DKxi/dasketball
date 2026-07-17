ac3b24db05b0d028202bd22d1eca23bd71a1947b
DKxi <mail2divij@gmail.com>
2026-07-17 16:32:39 -0400

    Change import from index.ts to index.js

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,6 +1,6 @@
 // Replace the old require() line with this:
 export default async function handler(req, res) {
-  const server = await import('../server/src/index.ts');
+  const server = await import('../server/src/index.js');
   
   // If your server exports a default handler, invoke it like this:
   return server.default(req, res);
